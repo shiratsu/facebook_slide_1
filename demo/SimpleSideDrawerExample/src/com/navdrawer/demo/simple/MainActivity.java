b4b3e0ec4ac6caf776b35c3c6d2778b675d9d768
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2013-01-03 10:31:19 +0900

    戻るパターンを追加

--- demo/SimpleSideDrawerExample/src/com/navdrawer/demo/simple/MainActivity.java
+++ demo/SimpleSideDrawerExample/src/com/navdrawer/demo/simple/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -16,8 +17,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mNav = new SimpleSideDrawer(this);
-        mNav.setBehindContentView(R.layout.activity_behind_simple);
+        View content = mNav.setBehindContentView(R.layout.activity_behind_simple);
+        Button test = (Button) content.findViewById(R.id.behind_btn);
+        
         findViewById(R.id.btn).setOnClickListener(new OnClickListener() {
+            @Override 
+            public void onClick(View v) {
+                mNav.toggleDrawer();
+            }
+        });
+        test.setOnClickListener(new OnClickListener() {
             @Override 
             public void onClick(View v) {
                 mNav.toggleDrawer();
